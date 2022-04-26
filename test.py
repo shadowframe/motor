ac225e3f86b4fe87d6bb1639021680dd9f650cce
Jan <jan.weber123@googlemail.com>
2022-04-26 10:55:38 +0200

    motor

--- test.py
+++ test.py
@@ -1,5 +1,34 @@
 import utime
+from machine import Pin
 
-while True:
-    print("Hi")
-    utime.sleep(1)
+motor1a = Pin(14, Pin.OUT)
+motor1b = Pin(15, Pin.OUT)
+
+
+def forward():
+    motor1a.high()
+    motor1b.low()
+
+
+def backward():
+    motor1a.low()
+    motor1b.high()
+
+
+def stop():
+    motor1a.low()
+    motor1b.low()
+
+
+def test():
+    forward()
+    utime.sleep(2)
+    backward()
+    utime.sleep(2)
+    stop()
+
+
+for i in range(5):
+    test()
+
+
